52df8f465e446c4b2fb8812141b1a77c8601dac7
Melissa Adlboller <melissa.adlboller@bulme.at>
2021-10-06 10:37:44 +0200

    interactivity

--- hello.cpp
+++ hello.cpp
@@ -1,9 +1,14 @@
 // Mein Hello-Programm
 
 #include <iostream>
+using namespace std;
 
 int main()
 {
-		std::cout << "Hallo 4BHEL!" << std::endl;
+		string name;
+		cout << "Hallo 4BHEL!" << endl;
+		cout << "Dein Name: ";
+		cin >> name;
+		cout << "Hallo " << name << endl;
 		return 0;
 }
